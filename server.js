9949e855752d55ae1a425a0169447893534de2ab
PRAVEEN2421 <praveenps20589@gmail.com>
2026-02-10 09:15:26 +0530

    initial backend commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import adminRouter from "./routes/adminRoute.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDB();
+connectCloudinary();
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://prescripto-hospital-management-system.onrender.com"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+
+app.use(express.json());
+
+// API routes
+app.use("/api/user", userRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on PORT:${port}`);
+});

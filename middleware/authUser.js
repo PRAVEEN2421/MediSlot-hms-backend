6569d232362d77ac53235d50d3fbba36f918bb5d
PRAVEEN2421 <praveenps20589@gmail.com>
2026-02-12 23:44:39 +0530

    Fixed authUser token destructuring issue

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -1,19 +1,25 @@
 import jwt from 'jsonwebtoken'
 
-// user authentication middleware
 const authUser = async (req, res, next) => {
-    const { token } = req.headers.authorization?.split(" ")[1]
-    if (!token) {
-        return res.json({ success: false, message: 'Not Authorized Login Again' })
-    }
     try {
+        const authHeader = req.headers.authorization
+
+        if (!authHeader) {
+            return res.json({ success: false, message: 'Not Authorized Login Again' })
+        }
+
+        const token = authHeader.split(" ")[1]
+
         const token_decode = jwt.verify(token, process.env.JWT_SECRET)
+
         req.body.userId = token_decode.id
+
         next()
+
     } catch (error) {
         console.log(error)
         res.json({ success: false, message: error.message })
     }
 }
 
-export default authUser;
+export default authUser

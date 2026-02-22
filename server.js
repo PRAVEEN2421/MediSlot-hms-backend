74ebc4cd61df3ed294219832c1713bfbc6112eb2
PRAVEEN2421 <praveenps20589@gmail.com>
2026-02-23 01:34:43 +0530

    Fix CORS issue for user token header

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "atoken", "dtoken"]
+  allowedHeaders: ["Content-Type", "Authorization", "atoken", "dtoken", "token"]
 }));
 
 app.options("*", cors());

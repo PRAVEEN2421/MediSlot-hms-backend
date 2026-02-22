09814723cf7ab0172950935865b0a08c9393fcc5
PRAVEEN2421 <praveenps20589@gmail.com>
2026-02-23 01:28:35 +0530

    Fix token validation in authUser middleware

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -3,17 +3,10 @@ import jwt from 'jsonwebtoken'
 
 const authUser = async (req, res, next) => {
     try {
-        const authHeader = req.headers.authorization
-
-      if (!authHeader.startsWith("Bearer ")) {
-   return res.status(401).json({
-      success: false,
-      message: "Invalid Token Format"
-   })
-}
-
-        const token = authHeader.split(" ")[1]
-
+        const { token } = req.headers
+        if (!token) {
+            return res.json({ success: false, message: 'Not Authorized Login Again' })
+        }
         const token_decode = jwt.verify(token, process.env.JWT_SECRET)
 
         req.body.userId = token_decode.id

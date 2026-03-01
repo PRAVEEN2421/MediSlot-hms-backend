aaa2714d324a14726fc2c797710c11a1a320a4f5
PRAVEEN2421 <praveenps20589@gmail.com>
2026-03-01 11:02:35 +0530

    Allow CORS requests from Netlify production frontend

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://medislot-hms-backend.onrender.com"
+    "https://medislot-hms-backend.onrender.com",
+    "https://docter-booking.netlify.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
